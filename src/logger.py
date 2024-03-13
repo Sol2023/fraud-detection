90980114a7c5f314f709be0b5ce5e108aaf0b982
Adam Huang <szhuang2021@gmail.com>
2024-03-12 20:52:26 -0500

    exception.py functions well

--- src/logger.py
+++ src/logger.py
@@ -8,17 +8,20 @@ os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(os.getcwd(), LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    level=logging.INFO,
-    format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
 
-)
+def setup_logging():
+    logging.basicConfig(
+        filename=LOG_FILE_PATH,
+        level=logging.INFO,
+        format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+
+    )
 
 
 if __name__ == "__main__":
-    logging.info("This is an info message")
-    logging.warning("This is a warning message")
-    logging.error("This is an error message")
-    logging.critical("This is a critical message")
+    setup_logging()
+    logging.info("Start logging")
+    # logging.warning("This is a warning message")
+    # logging.error("This is an error message")
+    # logging.critical("This is a critical message")
