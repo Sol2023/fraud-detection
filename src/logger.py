dadc39402b788cb17ad476480756fcbf9656712a
Adam Huang <szhuang2021@gmail.com>
2024-03-12 19:44:17 -0500

    logger.py test done

--- src/logger.py
+++ src/logger.py
@@ -11,7 +11,14 @@ LOG_FILE_PATH = os.path.join(os.getcwd(), LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO
+    format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+
 )
+
+
+if __name__ == "__main__":
+    logging.info("This is an info message")
+    logging.warning("This is a warning message")
+    logging.error("This is an error message")
+    logging.critical("This is a critical message")
